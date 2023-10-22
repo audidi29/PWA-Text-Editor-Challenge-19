96a3a2631a69212ca5efcbf2b3a447035e79732b
Your Name <you@example.com>
2023-10-21 20:18:43 -0700

    fixing code to make it works

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -1,3 +1,77 @@
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const WebpackPwaManifest = require('webpack-pwa-manifest');
+// const path = require('path');
+// const { InjectManifest } = require('workbox-webpack-plugin');
+
+// // TODO: Add and configure workbox plugins for a service worker and manifest file.
+// // TODO: Add CSS loaders and babel to webpack.
+
+// module.exports = () => {
+//   return {
+//     mode: 'development',
+//     entry: {
+//       main: './src/js/index.js',
+//       install: './src/js/install.js'
+//     },
+//     output: {
+//       filename: '[name].bundle.js',
+//       path: path.resolve(__dirname, 'dist'),
+//     },
+//     plugins: [
+
+// // Generate an HTML file with links to bundled JavaScript files
+//        new HtmlWebpackPlugin({
+//         template: './src/index.html', 
+//         chunks: ['main'], 
+//       }),
+
+// // Generate the Web App Manifest file 
+//       new WebpackPwaManifest({
+//         name: 'My PWA',
+//         short_name: 'MyPWA',
+//         description: 'A Progressive Web App',
+//         start_url: '/',
+//         background_color: '#ffffff',
+//         theme_color: '#007bff',
+//         icons: [
+//           {
+//             src: path.resolve('path-to-icon.png'),
+//             sizes: [96, 128, 192, 256, 384, 512], 
+//           },
+//         ],
+//       }),
+
+//       // Generate the service worker file using Workbox
+//       new InjectManifest({
+//         swSrc: './src/sw.js', 
+//         swDest: 'service-worker.js', 
+//       }),
+//     ],
+
+
+//     module: {
+//       rules: [
+//          // Add a rule for handling JavaScript with Babel
+//          {
+//           test: /\.js$/,
+//           exclude: /node_modules/,
+//           use: {
+//             loader: 'babel-loader',
+//             options: {
+//               presets: ['@babel/preset-env'],
+//             },
+//           },
+//         },
+//         // Add rules for handling CSS files
+//         {
+//           test: /\.css$/,
+//           use: ['style-loader', 'css-loader'],
+//         },
+//       ],
+//     },
+//   };
+// };
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
@@ -18,54 +92,50 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-
-// Generate an HTML file with links to bundled JavaScript files
-       new HtmlWebpackPlugin({
-        template: './src/index.html', 
-        chunks: ['main'], 
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'PWA'
       }),
-
-// Generate the Web App Manifest file 
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
       new WebpackPwaManifest({
-        name: 'My PWA',
-        short_name: 'MyPWA',
-        description: 'A Progressive Web App',
+        fingerprints: false,
+        inject: true,
+        name: 'index-db-pwa-19',
+        short_name: 'PWA',
+        description: 'highlighting note taking with JavaScript syntax!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
         start_url: '/',
-        background_color: '#ffffff',
-        theme_color: '#007bff',
+        publicPath: '/',
         icons: [
           {
-            src: path.resolve('path-to-icon.png'),
-            sizes: [96, 128, 192, 256, 384, 512], 
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
           },
         ],
       }),
-
-      // Generate the service worker file using Workbox
-      new InjectManifest({
-        swSrc: './src/sw.js', 
-        swDest: 'service-worker.js', 
-      }),
     ],
-
-
+// webpack is cool
     module: {
       rules: [
-         // Add a rule for handling JavaScript with Babel
-         {
-          test: /\.js$/,
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        }, 
+        {
+          test: /\.m?js$/,
           exclude: /node_modules/,
           use: {
             loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
             },
           },
-        },
-        // Add rules for handling CSS files
-        {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
         },
       ],
     },
