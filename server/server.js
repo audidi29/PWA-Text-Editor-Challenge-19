55e509ca53c6e01164a072e3cf3de37e8558a9af
Your Name <you@example.com>
2023-10-22 17:16:01 -0700

    Adding static middleware to server.js file

--- server/server.js
+++ server/server.js
@@ -1,15 +1,31 @@
+// const express = require('express');
+// const path = require('path');
+
+// const app = express();
+// const PORT = process.env.PORT || 3000;
+
+// app.use(express.static('../client/dist'));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// require('./routes/htmlRoutes')(app);
+
+// app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
+
+
 const express = require('express');
 const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('../client/dist'));
+// Define the absolute path to your static files
+const staticPath = path.join(__dirname, '../client/dist');
+
+app.use(express.static(staticPath));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
-
-
